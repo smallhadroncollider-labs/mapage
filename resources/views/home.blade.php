f60dc4cc8f485870cb8db8c7ff4d7d5e93a8a4b1
Mark Wales <mark@smallhadroncollider.com>
2015-02-25 22:59:25 +0000

    added basic markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('app')
 
 @section('content')
+<form id="js__message">
+    <div>
+        <textarea id="js__message-text" placeholder="Leave a message"></textarea>
+    </div>
+
+    <button>Submit</button>
+</form>
+
+<ul id="js__message-list"></ul>
+
+<script data-main="/js/load.js" src="/vendor/requirejs/require.js"></script>
 
 @endsection
